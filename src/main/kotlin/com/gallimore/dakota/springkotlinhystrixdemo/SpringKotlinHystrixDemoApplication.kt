4198f54f23c55ce85e47c90eadcdc39c609100f6
Dakota Gallimore <gallimoredakota@gmail.com>
2019-06-01 00:48:37 -0400

    initial commit

---
+++ src/main/kotlin/com/gallimore/dakota/springkotlinhystrixdemo/SpringKotlinHystrixDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.gallimore.dakota.springkotlinhystrixdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinHystrixDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinHystrixDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/gallimore/dakota/springkotlinhystrixdemo/SpringKotlinHystrixDemoApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.gallimore.dakota.springkotlinhystrixdemo
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class SpringKotlinHystrixDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
