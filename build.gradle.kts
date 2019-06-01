dd9d78ac88a17f82f907bf2476e3eb4758cce5c9
Dakota Gallimore <gallimoredakota@gmail.com>
2019-06-01 01:15:23 -0400

    Add Hystix & Hystix Dashboard to gradle build files and set server port

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,14 +15,24 @@ repositories {
 	mavenCentral()
 }
 
+extra["springCloudVersion"] = "Greenwich.SR1"
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-hystrix")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-hystrix-dashboard")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
 }
 
 tasks.withType<KotlinCompile> {
