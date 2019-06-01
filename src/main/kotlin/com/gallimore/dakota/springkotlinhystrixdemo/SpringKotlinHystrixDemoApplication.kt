c91a88eda32c563d8a525278e7195dc54306b766
Dakota Gallimore <gallimoredakota@gmail.com>
2019-06-01 01:21:24 -0400

    Enable Hystrix Dashboard and circuit breaker to service

--- src/main/kotlin/com/gallimore/dakota/springkotlinhystrixdemo/SpringKotlinHystrixDemoApplication.kt
+++ src/main/kotlin/com/gallimore/dakota/springkotlinhystrixdemo/SpringKotlinHystrixDemoApplication.kt
@@ -2,8 +2,12 @@ package com.gallimore.dakota.springkotlinhystrixdemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 class SpringKotlinHystrixDemoApplication
 
 fun main(args: Array<String>) {
